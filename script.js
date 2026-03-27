41c6eccbebb60e6c7717bf09e983d30f41bacc57
Yashwanth Marikanti <yashwanth2416@gmail.com>
2026-03-27 21:34:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
+function show(id){
+
+document.querySelectorAll('.section')
+.forEach(sec=>sec.style.display='none')
+
+document.getElementById(id).style.display='block'
+
+}
+
+function calc(){
+
+let w=document.getElementById("weight").value
+
+let protein=w*1.6
+
+document.getElementById("protein")
+.innerText="You need "+protein+"g protein daily"
+
+}
+
 function ask() {
     let q = document.getElementById("question").value.toLowerCase();
     let answer = "";
